631f343aba1534215a1bb0ad6a89de8f024ff7fd
ilmar <ilmar.danilo@querodelivery.com>
2023-02-21 10:21:54 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/**/index.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  transform: {
+    "\\.ts$": "ts-jest",
+  },
+};
